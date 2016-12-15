4b5c2f42f4ce0b715e9dc6b92c1eb8d63186ff77
varnagy-zsolt <varnagyzsolt1975@gmail.com>
2016-12-15 16:56:08 +0100

    substr_word(), sentence_trim() metodusok hozzaadva a string helperhez

--- system/helper/str.php
+++ system/helper/str.php
@@ -18,5 +18,45 @@ class Str
         $string = strtolower($string);
         return $string;
     }
+
+    /**
+     * Egy szövegből az elejétől kezdődően adott karakterszámú rész ad vissza szóra kerekítve
+     *  
+     * @param   string  $string  szöveg
+     * @param   int  $char  karakterek száma
+     * @return  string  a levágott szöveg
+     */
+    public function substr_word($string, $char)
+    {
+        $s = mb_substr($string, 0, $char, 'UTF-8');
+        return substr($s, 0, strrpos($s, ' '));
+    }
+
+    /**
+     * Egy szövegből az elejétől adott számú mondatot ad vissza
+     *  
+     * @param   string  $body  szöveg
+     * @param   int  $sentencesToDisplay  a mondatk száma
+     * @return  string  a levágott szöveg
+     */
+    public function sentence_trim($body, $sentencesToDisplay = 1)
+    {
+        $nakedBody = preg_replace('/\s+/', ' ', strip_tags($body));
+        $sentences = preg_split('/(\.|\?|\!)(\s)/', $nakedBody);
+
+        if (count($sentences) <= $sentencesToDisplay)
+            return $nakedBody;
+
+        $stopAt = 0;
+        foreach ($sentences as $i => $sentence) {
+            $stopAt += strlen($sentence);
+
+            if ($i >= $sentencesToDisplay - 1)
+                break;
+        }
+
+        $stopAt += ($sentencesToDisplay * 2);
+        return trim(substr($nakedBody, 0, $stopAt));
+    }    
 }
 ?>
