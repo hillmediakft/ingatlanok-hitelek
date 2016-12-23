42cfe716e54c9dc4f0cbce6a971cd69b69b1cfcc
varnagy-attila <vucuka@gmail.com>
2016-12-23 11:44:24 +0100

    admin settings módosítások
    Egyszerűsítettem egy kicsit a controllert, és kiegészítettem (valamint
    egyszerűsítettem) a view-t.

--- system/admin/view/settings/tpl_settings.php
+++ system/admin/view/settings/tpl_settings.php
@@ -1,84 +1,94 @@
 <div class="page-content">
-	<!-- BEGIN PAGE HEADER-->
-	<!-- BEGIN PAGE TITLE & BREADCRUMB-->
-	<div class="page-bar">
-		<ul class="page-breadcrumb">
-			<li>
-				<i class="fa fa-home"></i>
-				<a href="admin/home">Kezdőoldal</a> 
-				<i class="fa fa-angle-right"></i>
-			</li>
-			<li><span>Beállítások</span></li>
-		</ul>
-	</div>
+    <!-- BEGIN PAGE HEADER-->
+    <!-- BEGIN PAGE TITLE & BREADCRUMB-->
+    <div class="page-bar">
+        <ul class="page-breadcrumb">
+            <li>
+                <i class="fa fa-home"></i>
+                <a href="admin/home">Kezdőoldal</a> 
+                <i class="fa fa-angle-right"></i>
+            </li>
+            <li><span>Beállítások</span></li>
+        </ul>
+    </div>
     <!-- END PAGE TITLE & BREADCRUMB-->
-	<!-- END PAGE HEADER-->
+    <!-- END PAGE HEADER-->
 
     <div class="margin-bottom-20"></div>
 
-	<!-- BEGIN PAGE CONTENT-->
-	<div class="row">
-		<div class="col-md-12">
+    <!-- BEGIN PAGE CONTENT-->
+    <div class="row">
+        <div class="col-md-12">
 
             <!-- echo out the system feedback (error and success messages) -->
             <?php $this->renderFeedbackMessages(); ?>
 
-            <form action='' name='settings_form' id='settings_form' method='POST'>
-				<!-- BEGIN EXAMPLE TABLE PORTLET-->
-				<div class="portlet">
-					<div class="portlet-title">
-						<div class="caption"><i class="fa fa-cogs"></i>Beállítások szerkesztése</div>
-	                    <div class="actions">
+            <form action='' name='settings_form' id='form' method='POST'>
+                <!-- BEGIN EXAMPLE TABLE PORTLET-->
+                <div class="portlet">
+                    <div class="portlet-title">
+                        <div class="caption"><i class="fa fa-cogs"></i>Beállítások szerkesztése</div>
+                        <div class="actions">
                             <button class='btn green btn-sm' type='submit' name='submit_settings'><i class="fa fa-check"></i> Mentés</button>
                         </div>							
-					</div>
-					<div class="portlet-body">
+                    </div>
+                    <div class="portlet-body">
 
                         <div class="form-group">
-                            <label for="settings_ceg">Cég</label>	
-                            <input type='text' name='setting_ceg' class='form-control input-large' value="<?php echo (empty($settings['ceg'])) ? "" : $settings['ceg']; ?>"/>
+                            <label for="ceg">Cég/üzlet/iroda elnevezése</label>	
+                            <input type='text' name='ceg' class='form-control input-xlarge' value="<?php echo (empty($settings['ceg'])) ? "" : $settings['ceg']; ?>"/>
                         </div>
 
                         <div class="form-group">
-                            <label for="settings_cim">Cím</label>	
-                            <input type='text' name='setting_cim' class='form-control input-large' value="<?php echo (empty($settings['cim'])) ? "" : $settings['cim']; ?>"/>
+                            <label for="cim">Cím</label>	
+                            <input type='text' name='cim' class='form-control input-xlarge' value="<?php echo (empty($settings['cim'])) ? "" : $settings['cim']; ?>"/>
                         </div>
 
                         <div class="form-group">
-                            <label for="settings_tel">Telefonszám</label>	
-                            <input type='text' name='setting_tel' class='form-control input-large' value="<?php echo (empty($settings['tel'])) ? "" : $settings['tel']; ?>"/>
+                            <label for="tel">Telefonszám</label>	
+                            <input type='text' name='tel' class='form-control input-xlarge' value="<?php echo (empty($settings['tel'])) ? "" : $settings['tel']; ?>"/>
                         </div>
 
                         <div class="form-group">
-                            <label for="settings_email">E-mail (lábléc e-mail űrlap)</label>	
-                            <input type='text' name='setting_email' class='form-control input-large' value="<?php echo (empty($settings['email'])) ? "" : $settings['email']; ?>"/>
+                            <label for="email">E-mail (lábléc e-mail űrlap)</label>	
+                            <input type='text' name='email' class='form-control input-xlarge' value="<?php echo (empty($settings['email'])) ? "" : $settings['email']; ?>"/>
                         </div>
+
+
+                        <div class="form-group">
+                            <label for="facebook">Facebook fiók url-je</label>	
+                            <input type='text' name='facebook' class='form-control input-xlarge' value="<?php echo (empty($settings['facebook'])) ? "" : $settings['facebook']; ?>"/>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="googleplus">Google plus fiók url-je</label>	
+                            <input type='text' name='googleplus' class='form-control input-xlarge' value="<?php echo (empty($settings['googleplus'])) ? "" : $settings['googleplus']; ?>"/>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="twitter">Twitter fiók url-je</label>	
+                            <input type='text' name='twitter' class='form-control input-xlarge' value="<?php echo (empty($settings['twitter'])) ? "" : $settings['twitter']; ?>"/>
+                        </div>   
+
+                        <div class="form-group">
+                            <label for="linkedin">Linkedin fiók url-je</label>	
+                            <input type='text' name='linkedin' class='form-control input-xlarge' value="<?php echo (empty($settings['linkedin'])) ? "" : $settings['linkedin']; ?>"/>
+                        </div> 
                         
-                    <!--
                         <div class="form-group">
-                            <label for="setting_email_ceges">E-mail céges (céges kapcsolat)</label>	
-                            <input type='text' name='setting_email_ceges' class='form-control input-large' value="<?php //echo (empty($settings['email_ceges'])) ? "" : $settings['email_ceges']; ?>"/>
-                        </div>
-                           
-                        <div class="form-group">
-                            <label for="setting_email_diak">E-mail diák</label>	
-                            <input type='text' name='setting_email_diak' class='form-control input-large' value="<?php //echo (empty($settings['email_diak'])) ? "" : $settings['email_diak']; ?>"/>
-                        </div>
-                                                    
-                        <div class="form-group">
-                            <label for="setting_email_kilepes">E-mail kilépés</label>	
-                            <input type='text' name='setting_email_kilepes' class='form-control input-large' value="<?php //echo (empty($settings['email_kilepes'])) ? "" : $settings['email_kilepes']; ?>"/>
-                        </div>                                                            
+                            <label for="pagination">Megjelenített elemek száma oldalanként</label>  
+                            <select name="pagination" class="form-control input-xlarge">
+                                <?php for ($i = 6; $i < 22 ; $i += 3) {
+                                    echo '<option value="' . $i . '"';
+                                    echo ($settings['pagination'] == $i) ? 'selected' : '';
+                                    echo '>' . $i . '</option>' . "\r\n";
+                                } ?>
+                            </select>
+                        </div>                         
 
-                        <div class="form-group">
-                            <label for="setting_email_diak">Facebook link</label>	
-                            <input type='text' name='setting_facebook_link' class='form-control input-large' value="<?php //echo (empty($settings['facebook_link'])) ? "" : $settings['facebook_link']; ?>"/>
-                        </div>
-                    -->
-						
-					</div> <!-- END USER GROUPS PORTLET BODY-->
-				</div> <!-- END USER GROUPS PORTLET-->
+                    </div> <!-- END USER GROUPS PORTLET BODY-->
+                </div> <!-- END USER GROUPS PORTLET-->
             </form>
-		</div> <!-- END COL-MD-12 -->
-	</div> <!-- END ROW -->	
+        </div> <!-- END COL-MD-12 -->
+    </div> <!-- END ROW -->	
 </div> <!-- END PAGE CONTENT-->
