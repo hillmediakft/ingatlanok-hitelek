6f447262163de1f3ee2ec172e7f9666cc50222b0
varnagy-zsolt <varnagyzsolt1975@gmail.com>
2017-01-14 16:53:19 +0100

    sidebar referensek modul template modositva

--- system/Site/view/ingatlanok/tpl_modul_referenscontact.php
+++ system/Site/view/ingatlanok/tpl_modul_referenscontact.php
@@ -1,6 +1,7 @@
+<?php use System\Libs\Language as Lang; ?>
 <div class="widget agents">
     <div class="heading">
-        <span class="widget-title">Our Agents</span>
+        <span class="widget-title"><?php echo Lang::get('menu_ertekesitoink'); ?></span>
         <div class="jcarousel-arrows">
             <a href="#" class="prev-slide"><i class="fa fa-angle-left"></i></a>
             <a href="#" class="next-slide"><i class="fa fa-angle-right"></i></a>
@@ -9,126 +10,44 @@
     <div class="widget-entry">
         <div class="wg-ag-carousel carousel">
             <ul>
-                <li>
-                    <div class="item">
-                        <div class="item-heading">
-                            <div class="preview">
-                                <img src="media/images/agency/1.png" alt="">
-                                <div class="overlay">
-                                    <a href="agent-single_agent.html"><i class="fa fa-search-plus"></i></a>
+
+                <?php foreach ($agents as $agent) { ?>
+                    <li>
+                        <div class="item">
+                            <div class="item-heading">
+                                <div class="preview" style="overflow: hidden;">
+                                    <img src="<?php echo $this->getConfig('user.upload_path') . $agent['photo']; ?>" alt="">
+                                    <!--
+                                    <div class="overlay">
+                                        <a href="agent-single_agent.html"><i class="fa fa-search-plus"></i></a>
+                                    </div>
+                                    -->
+                                </div>
+                                <div class="descr">
+                                    <span class="name"><?php echo (LANG == 'hu') ? $agent['first_name'] . ' '. $agent['last_name'] : $agent['last_name'] . ' ' . $agent['first_name']; ?></span>
+                                    <span class="properties"><?php echo $agent['property']; ?> ingatlan</span>
                                 </div>
                             </div>
-                            <div class="descr">
-                                <span class="name">Djohn Kollings</span>
-                                <span class="properties">5 properties</span>
-                            </div>
+                            <ul class="contact-listing">
+                                <li>
+                                    <span class="icon"><i class="fa fa-phone"></i></span>
+                                    <span class="phone"><?php echo $agent['phone']; ?></span>
+                                </li>
+                                <li>
+                                    <span class="icon"><i class="fa fa-envelope"></i></span>
+                                    <a href="#" class="mail"><?php echo $agent['email']; ?></a>
+                                </li>
+                                <!--
+                                <li>
+                                    <span class="icon"><i class="fa fa-globe"></i></span>
+                                    <a href="#" class="site">infoexample.com</a>
+                                </li>
+                                -->
+                            </ul>
                         </div>
-                        <ul class="contact-listing">
-                            <li>
-                                <span class="icon"><i class="fa fa-phone"></i></span>
-                                <span class="phone">+234-754-596</span>
-                            </li>
-                            <li>
-                                <span class="icon"><i class="fa fa-envelope"></i></span>
-                                <a href="#" class="mail">info@example.com</a>
-                            </li>
-                            <li>
-                                <span class="icon"><i class="fa fa-globe"></i></span>
-                                <a href="#" class="site">infoexample.com</a>
-                            </li>
-                        </ul>
-                    </div>
-                </li>
-                <li>
-                    <div class="item">
-                        <div class="item-heading">
-                            <div class="preview">
-                                <img src="media/images/agency/2.png" alt="">
-                                <div class="overlay">
-                                    <a href="agent-single_agent.html"><i class="fa fa-search-plus"></i></a>
-                                </div>
-                            </div>
-                            <div class="descr">
-                                <span class="name">Eline Dorther</span>
-                                <span class="properties">3 properties</span>
-                            </div>
-                        </div>
-                        <ul class="contact-listing">
-                            <li>
-                                <span class="icon"><i class="fa fa-phone"></i></span>
-                                <span class="phone">+234-754-596</span>
-                            </li>
-                            <li>
-                                <span class="icon"><i class="fa fa-envelope"></i></span>
-                                <a href="#" class="mail">info@example.com</a>
-                            </li>
-                            <li>
-                                <span class="icon"><i class="fa fa-globe"></i></span>
-                                <a href="#" class="site">infoexample.com</a>
-                            </li>
-                        </ul>
-                    </div>
-                </li>
-                <li>
-                    <div class="item">
-                        <div class="item-heading">
-                            <div class="preview">
-                                <img src="media/images/agency/3.png" alt="">
-                                <div class="overlay">
-                                    <a href="agent-single_agent.html"><i class="fa fa-search-plus"></i></a>
-                                </div>
-                            </div>
-                            <div class="descr">
-                                <span class="name">Erik Braun</span>
-                                <span class="properties">8 properties</span>
-                            </div>
-                        </div>
-                        <ul class="contact-listing">
-                            <li>
-                                <span class="icon"><i class="fa fa-phone"></i></span>
-                                <span class="phone">+234-754-596</span>
-                            </li>
-                            <li>
-                                <span class="icon"><i class="fa fa-envelope"></i></span>
-                                <a href="#" class="mail">info@example.com</a>
-                            </li>
-                            <li>
-                                <span class="icon"><i class="fa fa-globe"></i></span>
-                                <a href="#" class="site">infoexample.com</a>
-                            </li>
-                        </ul>
-                    </div>
-                </li>
-                <li>
-                    <div class="item">
-                        <div class="item-heading">
-                            <div class="preview">
-                                <img src="media/images/agency/4.png" alt="">
-                                <div class="overlay">
-                                    <a href="agent-single_agent.html"><i class="fa fa-search-plus"></i></a>
-                                </div>
-                            </div>
-                            <div class="descr">
-                                <span class="name">Djoanna Holl</span>
-                                <span class="properties">5 properties</span>
-                            </div>
-                        </div>
-                        <ul class="contact-listing">
-                            <li>
-                                <span class="icon"><i class="fa fa-phone"></i></span>
-                                <span class="phone">+234-754-596</span>
-                            </li>
-                            <li>
-                                <span class="icon"><i class="fa fa-envelope"></i></span>
-                                <a href="#" class="mail">info@example.com</a>
-                            </li>
-                            <li>
-                                <span class="icon"><i class="fa fa-globe"></i></span>
-                                <a href="#" class="site">infoexample.com</a>
-                            </li>
-                        </ul>
-                    </div>
-                </li>
+                    </li>
+                <?php } ?>
+
             </ul>
         </div>
     </div>
