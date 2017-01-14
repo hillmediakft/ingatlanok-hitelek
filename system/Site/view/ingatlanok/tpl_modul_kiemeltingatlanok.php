04811ef37a4a76dcd1fbed3fbbb3f9c08f018d59
varnagy-zsolt <varnagyzsolt1975@gmail.com>
2017-01-14 15:26:43 +0100

    javitas

--- system/Site/view/ingatlanok/tpl_modul_kiemeltingatlanok.php
+++ system/Site/view/ingatlanok/tpl_modul_kiemeltingatlanok.php
@@ -4,16 +4,16 @@
 		<span class="widget-title"><?php echo Lang::get('home_kiemelt_ingatlanok'); ?></span>
 	</div>
 	<div class="widget-entry">
-
 		<?php
 			foreach ($kiemelt_ingatlanok as $ingatlan) {
 				$photo_array = json_decode($ingatlan['kepek']);
 			?>
 				<div class="single-prop">
+					<span style="position:absolute; padding: 1px 4px; top: 0px; background-color: #C44A35; color: #ffffff;"><?php echo($ingatlan['tipus'] == 1) ? Lang::get('kereso_elado') : Lang::get('kereso_kiado'); ?></span>
 					<div class="preview">
 						<div style="overflow: hidden; width:85px; height:85px;">
 							<?php if ($ingatlan['kepek']) { ?>
-							    <img style="height:85px; width:auto;" src="<?php echo $this->url_helper->thumbPath($this->getConfig('ingatlan_photo.upload_path') . $photo_array[0]); ?>" alt="<?php echo $ingatlan['ingatlan_nev_' . LANG]; ?>">
+							    <img style="height:85px; width:auto;" src="<?php echo $this->url_helper->thumbPath($this->getConfig('ingatlan_photo.upload_path') . $photo_array[0], null, 'small'); ?>" alt="<?php echo $ingatlan['ingatlan_nev_' . LANG]; ?>">
 							<?php } else { ?>
 							    <img style="height:85px; width:auto;" src="<?php echo Config::get('ingatlan_photo.upload_path') . 'placeholder.jpg'; ?>" alt="<?php echo $ingatlan['ingatlan_nev']; ?>">
 							<?php } ?>
