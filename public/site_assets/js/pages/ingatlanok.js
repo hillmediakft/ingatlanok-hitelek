09c27c735780fe61360e1e5ca48f5b52353c8c15
varnagy-zsolt <varnagyzsolt1975@gmail.com>
2017-01-14 16:53:47 +0100

    uj elem hozzaadva (egyenlore kikommentelve)

--- public/site_assets/js/pages/ingatlanok.js
+++ public/site_assets/js/pages/ingatlanok.js
@@ -4,12 +4,26 @@ var Ingatlanok = function () {
         setTimeout(function () {
             $('#equalheight-property-list div.item').equalHeights();
         }, 200);
-    }
+    };
+
+    /* ********************** Listázási sorrend módosítása ************************* */
+    /*
+    var setOrder = function () {
+        $('#sorrend_div').on('change', '#ui-id-1', function () {
+            url = $("#ui-id-1 option:selected").val();
+            //window.location.href = location.protocol + "//" + location.host + url;
+
+            console.log(url);
+        });
+    };
+    */
+
 
     return {
         //main function to initiate the module
         init: function () {
             equalHeights();
+            setOrder();
         }
     };
 }();
