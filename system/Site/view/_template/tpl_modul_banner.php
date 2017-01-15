bec4a95605eda50c9d4d70bf7183e08e93f41c46
varnagy-attila <vucuka@gmail.com>
2017-01-15 22:19:39 +0100

    sidebar modulok módosítások

---
+++ system/Site/view/_template/tpl_modul_banner.php
@@ -0,0 +1,10 @@
+<div class="widget banner">
+    <div class="banner-img">
+        <img src="<?php echo SITE_IMAGE;?>sidebar-banner.jpg" alt="">
+    </div>
+    <div class="banner-entry">
+        <span class="banner-title">Lorem ipsum</span>
+        <span class="banner-sub">Lorem ipsum doles ementia prentis upset</span>
+        <a href="#" class="learn-more">Tovább</a>
+    </div>
+</div>
