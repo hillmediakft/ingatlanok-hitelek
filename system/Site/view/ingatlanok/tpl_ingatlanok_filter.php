4ac2479635f83e84b5385cc62f754c54dbcbd308
varnagy-zsolt <varnagyzsolt1975@gmail.com>
2017-01-16 16:08:09 +0100

    sima bootstrap elemek a keresoben

--- system/Site/view/ingatlanok/tpl_ingatlanok_filter.php
+++ system/Site/view/ingatlanok/tpl_ingatlanok_filter.php
@@ -1,3 +1,4 @@
+<?php use System\Libs\Language as Lang; ?>
 <div class="widget main-filter-widget gray-bg">
     <div class="widget-entry">
         <div class="filter-label-block">
@@ -5,106 +6,99 @@
         </div>
         <form action="#" class="filter-form">
             <div class="row">
-                
                 <div class="col-sm-12">
-                    <span class="item-label">Eladó / Kiadó</span>
-                    <div id="select-type-holder4" class="ui-front">
-                        <select name="prop-select" data-icon="false" class="select filter-select">
-                            <option selected="selected">Any</option>
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
+                    
+                    <span class="item-label"><?php echo Lang::get('kereso_elado'); ?>/<?php echo Lang::get('kereso_kiado'); ?></span>
+                    <div class="form-group">
+                        <select name="tipus" class="form-control">
+                            <option selected="selected" value="1"><?php echo Lang::get('kereso_elado'); ?></option>
+                            <option value="2"><?php echo Lang::get('kereso_kiado'); ?></option>
                         </select>
-                    </div>  
-                </div>
+                    </div> 
 
-            </div>
-            <div class="row">
-                
-                <div class="col-sm-12">
-                    <span class="item-label">Location</span>
-                    <div id="select-type-holder5" class="ui-front">
-                        <select name="prop-select" data-icon="false" class="select filter-select">
-                            <option selected="selected">Any</option>
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                        </select>
-                    </div>  
-                </div>
-                
-            </div>
-            <div class="row">
-                <div class="col-sm-12">
-                    <span class="item-label">Property Status</span>
-                    <div id="select-type-holder6" class="ui-front">
-                        <select name="prop-select" data-icon="false" class="select filter-select">
-                            <option selected="selected">Any</option>
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                        </select>
-                    </div>  
                 </div>
             </div>
             <div class="row">
                 <div class="col-sm-12">
-                    <span class="item-label">Area <span class="label-descr">(Sq Ft)</span></span>
-                    <div id="select-type-holder7" class="ui-front">
-                        <select name="prop-select" data-icon="false" class="select filter-select">
-                            <option selected="selected">Any</option>
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
+
+                    <span class="item-label"><?php echo Lang::get('kereso_varos'); ?></span>
+                    <div class="form-group">
+                        <select name="varos" id="varos" class="form-control" >
+                            <option value="">-- <?php echo Lang::get('kereso_mindegy'); ?> --</option>;
+                            <?php echo $city_list; ?>
                         </select>
-                    </div>  
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-sm-6">
-                    <span class="item-label">Beds</span>
-                    <div id="select-type-holder8" class="ui-front">
-                        <select name="prop-select" data-icon="false" class="select filter-select">
-                            <option selected="selected">Any</option>
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                        </select>
-                    </div>  
-                </div>
-                <div class="col-sm-6">
-                    <span class="item-label">Baths</span>
-                    <div id="select-type-holder9" class="ui-front">
-                        <select name="prop-select" data-icon="false" class="select filter-select">
-                            <option selected="selected">Any</option>
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                        </select>
-                    </div>  
+                    </div>
+
                 </div>
             </div>
             <div class="row">
                 <div class="col-sm-12">
-                    <span class="item-label">Price</span>
-                    <div class="range-wrap">
-                        <div class="range-fields">
-                            <input type="text" id="min-field"/>
-                            <span class="delimiter"></span>
-                            <input type="text" id="max-field"/>
-                        </div>
-                        <div class="slider"></div>
-                        <div class="scale">
-                            <span class="min-value"></span>
-                            <span class="max-value"></span>
+
+                    <span class="item-label"><?php echo Lang::get('kereso_kerulet'); ?></span>
+                    <div class="form-group">
+                        <select disabled="disabled" id="district" name="kerulet" class="form-control" >
+                            <option value="">-- <?php echo Lang::get('kereso_mindegy'); ?> --</option>
+                            <?php echo $district_list; ?>
+                        </select>
+                    </div>
+ 
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12">
+
+                    <span class="item-label"><?php echo Lang::get('kereso_kategoria'); ?></span>
+                    <div class="form-group">
+                        <select name="kategoria" class="form-control">
+                            <option value="">-- <?php echo Lang::get('kereso_mindegy'); ?> --</option>
+                            <?php foreach ($category_list as $value) : ?>
+                                <option value="<?php echo $value['kat_id']; ?>"><?php echo $value['kat_nev_' . LANG]; ?></option>
+                                <?php endforeach ?>
+                        </select>
+                    </div>
+
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12">
+
+                    <span class="item-label"><?php echo Lang::get('kereso_min_ar'); ?></span>
+                    <div class="form-group">
+                        <div class="input-group">
+                            <input name="min_ar" type="text" class="form-control">
+                            <div class="input-group-addon">Ft</div>
                         </div>
                     </div>
+
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12">
+
+                    <span class="item-label"><?php echo Lang::get('kereso_max_ar'); ?></span>
+                    <div class="form-group">
+                        <div class="input-group">
+                            <input name="max_ar" type="text" class="form-control">
+                            <div class="input-group-addon">Ft</div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-sm-12">
+                    
+                    <span class="item-label"><?php echo Lang::get('kereso_alapterulet'); ?></span>
+                    <div class="form-group">
+                        <input name="min_terulet" type="text" class="form-control">
+                    </div>
+
                 </div>
             </div>
             <div class="row">
                 <div class="col-sm-12">
                     <nav id="rotate_btn_find" class="rotate_btn_find">
-                        <button class="find-now-btn">Find Now</button>
+                        <button class="find-now-btn"><?php echo Lang::get('kereso_kereses'); ?></button>
                     </nav>
                 </div>
             </div>
