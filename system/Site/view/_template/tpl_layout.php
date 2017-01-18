aae36deebaa8dc727d352660d4ec0e9ee8bece5f
varnagy-zsolt <varnagyzsolt1975@gmail.com>
2017-01-18 17:18:03 +0100

    modal-okhoz kapcsolodo js es css elemek a layoutba

--- system/Site/view/_template/tpl_layout.php
+++ system/Site/view/_template/tpl_layout.php
@@ -13,11 +13,12 @@
         <base href="<?php echo BASE_URL; ?>">
         <!-- BEGIN GLOBAL MANDATORY STYLES -->
         <!-- <link href="//fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css" /> -->
-        <link rel="stylesheet" type="text/css" media="screen" href="<?php echo SITE_CSS; ?>bootstrap.min.css">
+                  <link rel="stylesheet" type="text/css" media="screen" href="<?php echo SITE_ASSETS; ?>vendors/bootstrap/css/bootstrap.min.css">
         <link rel="stylesheet" type="text/css" media="screen" href="<?php echo SITE_ASSETS; ?>fonts/font-awesome-4.4.0/css/font-awesome.min.css">
         <link rel="stylesheet" type="text/css" media="screen" href="<?php echo SITE_ASSETS; ?>vendors/jquery-ui-1.11.4/jquery-ui.min.css">
         <link rel="stylesheet" type="text/css" media="screen" href="<?php echo SITE_ASSETS; ?>vendors/jcarousel/css/jquery.jcarousel.css">
         <link rel="stylesheet" type="text/css" media="screen" href="<?php echo $this->url_helper->autoversion(SITE_CSS . 'main-red.css');?>" />
+        <link rel="stylesheet" type="text/css" media="screen" href="<?php echo $this->url_helper->autoversion(SITE_CSS . 'custom.css');?>" />
         <link rel="shortcut icon" href="<?php echo SITE_IMAGE; ?>favicon.ico" />
         <!-- END GLOBAL MANDATORY STYLES -->
 
@@ -44,12 +45,25 @@
        
         
         <script type="text/javascript" src="<?php echo SITE_JS; ?>jquery.min.js"></script>
-	<script type="text/javascript" src="<?php echo SITE_JS; ?>main.js"></script>
-	<script type="text/javascript" src="<?php echo SITE_ASSETS; ?>vendors/jquery-ui-1.11.4/jquery-ui.min.js"></script>
-	<script type="text/javascript" src="<?php echo SITE_ASSETS; ?>vendors/languageswitcher/languageswitcher.js"></script>
-	<script type="text/javascript" src="<?php echo SITE_ASSETS; ?>vendors/jcarousel/js/jquery.jcarousel.min.js"></script>
+    	<script type="text/javascript" src="<?php echo SITE_JS; ?>main.js"></script>
+    	<script type="text/javascript" src="<?php echo SITE_ASSETS; ?>vendors/jquery-ui-1.11.4/jquery-ui.min.js"></script>
+    	<script type="text/javascript" src="<?php echo SITE_ASSETS; ?>vendors/languageswitcher/languageswitcher.js"></script>
+    	<script type="text/javascript" src="<?php echo SITE_ASSETS; ?>vendors/jcarousel/js/jquery.jcarousel.min.js"></script>
         <script type="text/javascript" src="<?php echo SITE_ASSETS; ?>vendors/equalheights/jquery.equalheights.min.js"></script>
-	
+        
+    <!-- Minden oldalon szükséges elemek -->
+        <!-- BOOTSTRAP JS -->
+                <script type="text/javascript" src="<?php echo SITE_ASSETS; ?>vendors/bootstrap/js/bootstrap.min.js"></script>
+        <!-- MODAL handler -->
+        <script type="text/javascript" src="<?php echo SITE_JS; ?>pages/modal_handler.js"></script>
+        <!-- Form validátor -->
+                <script type="text/javascript" src="<?php echo SITE_ASSETS; ?>vendors/jquery-validation/jquery.validate.min.js"></script>
+                <script type="text/javascript" src="<?php echo SITE_ASSETS; ?>vendors/jquery-validation/additional-methods.min.js"></script>
+                <script type="text/javascript" src="<?php echo SITE_ASSETS; ?>vendors/jquery-validation/localization/messages_hu.js"></script>
+                <script type="text/javascript" src="<?php echo SITE_ASSETS; ?>vendors/jquery-validation/localization/methods_hu.js"></script>
+        <!-- Block UI -->
+                <script type="text/javascript" src="<?php echo SITE_ASSETS; ?>vendors/jquery.blockui.min.js"></script>
+                	
         <!-- BEGIN PAGE LEVEL SCRIPTS -->
         <?php $this->get_js_link(); ?>
         <!-- END PAGE LEVEL SCRIPTS -->
