0247ea62a7eed32fc80fec8079bb4740bcdf0791
varnagy-zsolt <varnagyzsolt1975@gmail.com>
2017-01-26 19:25:53 +0100

    keresesekre vonatkozo elemek atkerultek a handle_search.js -be

--- public/site_assets/js/pages/ingatlanok.js
+++ public/site_assets/js/pages/ingatlanok.js
@@ -6,59 +6,10 @@ var Ingatlanok = function () {
         }, 200);
     };
 
-    /**
-     * Kerület lista kezelése
-     */
-    var enableDistrict = function () {
-
-        var option_value = $("select#varos option:selected").val();
-
-        if (option_value == '88') {
-            $('#district').prop("disabled", false);
-        }
-
-        //kerület és városrész option lista megjelenítése, ha a kiválasztott megye Budapest
-        $("#varos").change(function () {
-            
-            //option listaelem tartalom
-            //var str = $("select#varos option:selected").text();
-            // option listaelem value
-            option_value = $("select#varos option:selected").val();
-            // az érték üres lesz, ha a válassz elemet választjuk ki az option listából
-
-            if (option_value == '88') {
-                $('#district').prop("disabled", false);
-
-            } else {
-                $('#district option[value=""]').prop('selected', true);
-                $('#district').prop("disabled", true);
-            }
-        })
-    };
-
-
-    /* ********************** Listázási sorrend módosítása ************************* */
-    var setOrder = function () {
-        $('#sorrend_select').on('change', function () {
-            url = $("#sorrend_select option:selected").val();
-            window.location.href = location.protocol + "//" + location.host + url;
-        });
-    };
-
-        /* ********************** Reset filter ************************* */
-        var resetFilter = function () {
-            $('#reset-filter').on('click', function () {
-                window.location.href = location.protocol + "//" + location.host + '/ingatlanok';
-            });
-        };
-
-
     return {
         //main function to initiate the module
         init: function () {
-            enableDistrict();
             equalHeights();
-            setOrder();
         }
     };
 }();
