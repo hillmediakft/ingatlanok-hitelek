ba894b362f4e2831b71bb7fa43634aae714f9ce1
varnagy-zsolt <varnagyzsolt1975@gmail.com>
2017-02-02 13:18:01 +0100

    Number helper letrehozva

---
+++ system/Helper/Num.php
@@ -0,0 +1,40 @@
+<?php
+namespace System\Helper;
+/**
+* Number helper
+*/
+class Num {
+
+	/**
+	 * Nagy számokat alakítja át rövidebb formára 2 000 000 - 2 M
+	 */
+    public function niceNumber($n, $scale = true)
+    {
+		// a nem numerikus karaktereket üres stringre cseréljük (ha üres string marad, akkor 0 lesz)
+        $n = preg_replace('~\D~', '', $n);
+
+        // is this a number?
+        if (!is_numeric($n)) {
+            return false;
+        }
+
+        if ($n >= 1000000) {
+			$n = round(($n / 1000000), 2);
+			if ($scale) {
+				$n .= '&nbsp;M';
+			}	
+            return $n;
+        }
+        elseif ($n > 1000) {
+			$n = round(($n / 1000), 0);
+            if ($scale) {
+				$n .= '&nbsp;E';
+			}
+			return $n;
+        }
+
+        return number_format($n);
+    }
+
+}
+?>
