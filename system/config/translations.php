5c9168b23d5cbd6060d433c2c98c7aaabf9d70ae
varnagy-zsolt <varnagyzsolt1975@gmail.com>
2017-02-02 16:27:58 +0100

    translations config file letrehozva

---
+++ system/config/translations.php
@@ -0,0 +1,6 @@
+<?php 
+$config['orientation'] = array(
+    'hu' => array('észak', 'északkelet', 'kelet', 'délkelet', 'dél', 'délnyugat', 'nyugat', 'északnyugat'),
+    'en' => array('north', 'northeast', 'east', 'southeast', 'south', 'southwest', 'west', 'northwest')
+);
+?>
