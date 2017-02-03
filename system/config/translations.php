996e0cc62731c01c2cd24117bc37fa6a1c097bb4
varnagy-zsolt <varnagyzsolt1975@gmail.com>
2017-02-03 14:47:59 +0100

    felesleges elem eltavolitva

--- system/config/translations.php
+++ system/config/translations.php
@@ -3,45 +3,4 @@ $config['orientation'] = array(
     'hu' => array('észak', 'északkelet', 'kelet', 'délkelet', 'dél', 'délnyugat', 'nyugat', 'északnyugat'),
     'en' => array('north', 'northeast', 'east', 'southeast', 'south', 'southwest', 'west', 'northwest')
 );
-
-$config['emelet'] = array(
-	'hu' => array(
-		'alagsor',
-		'pince',
-		'szuterén',
-		'földszint',
-		'magasföldszint',
-		'félemelet',
-		'1. emelet',
-		'2. emelet',
-		'3. emelet',
-		'4. emelet',
-		'5. emelet',
-		'6. emelet',
-		'7. emelet',
-		'8. emelet',
-		'9. emelet',
-		'10. emelet',
-		'10. emelet felett'
-	),
-	'en' => array(
-		'basement',
-		'cellar',
-		'szuterén',
-		'ground floor',
-		'high ground floor',
-		'mezzanine',
-		'1th floor',
-		'2th floor',
-		'3th floor',
-		'4th floor',
-		'5th floor',
-		'6th floor',
-		'7th floor',
-		'8th floor',
-		'9th floor',
-		'10th floor',
-		'above 10th floor'
-	)
-);
 ?>
