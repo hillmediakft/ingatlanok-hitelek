f853a7505831344c8fc375a0054f8e51fe84453b
varnagy-zsolt <varnagyzsolt1975@gmail.com>
2017-02-06 11:33:06 +0100

    ckeditor minimal1 config modositva

--- public/admin_assets/plugins/ckeditor/config_minimal1.js
+++ public/admin_assets/plugins/ckeditor/config_minimal1.js
@@ -5,7 +5,7 @@
 
 CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
-	 config.language = 'hu';
+	config.language = 'hu';
 	 //config.uiColor = '#AADC6E';
 	
 	// szélesség és magasság beállítása
@@ -14,26 +14,23 @@ CKEDITOR.editorConfig = function( config ) {
 
 	//config.templates_files = [ 'public/admin_assets/plugins/ckeditor/my_templates/mytemplates.js' ];
 	config.templates_files = [ 'public/admin_assets/plugins/ckeditor/plugins/templates/my_templates/mytemplates.js' ];
-        config.templates_replaceContent = false;
+    
+	//csak a 'kicseréli a jelenlegi tartalmat' checkbox állapotát állítja be
+    config.templates_replaceContent = false;
 
-		//csak a 'kicseréli a jelenlegi tartalmat' checkbox állapotát állítja be
-		//config.templates_replaceContent = false;
-
-	
-	
-config.toolbar = [
-	{ name: 'alap_formazas', items: [ 'Bold', 'Italic' ] },
-	{ name: 'igazitas', items: [ 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock' ] },
-	{ name: 'listak', items: [ 'BulletedList', 'NumberedList' ] },
-	{ name: 'visszavonas', items: [ 'Undo', 'Redo' ] },
-	{ name: 'linkek', items: [ 'Link', 'Unlink' ] },
-	{ name: 'tools', items: [ 'HorizontalRule', 'SpecialChar' ] },
-	{ name: 'formazas', items: [ 'Format' ] }
+	// toolbar config	
+	config.toolbar = [
+		{ name: 'basicstyles', items: [ 'Bold', 'Italic' ] },
+		{ name: 'paragraph', items: [ 'NumberedList', 'BulletedList', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock' ] },
+		{ name: 'clipboard', items: [ 'Cut', 'Copy', 'Paste', '-', 'Undo', 'Redo' ] },
+		{ name: 'links', items: [ 'Link', 'Unlink' ] },
+		{ name: 'tools', items: [ 'HorizontalRule', 'SpecialChar' ] },
+		{ name: 'styles', items: [ 'Format' ] }
 	];	
-	
 
 	// Gombok eltávolítása az eszköztárból
-	config.removeButtons = 'Underline,Subscript,Superscript';
+	//config.removeButtons = 'Underline,Subscript,Superscript';
+	
 	// minden html elem engedélyezése (nem távolít el semmit)
 	config.allowedContent = true;
 
@@ -44,16 +41,19 @@ config.toolbar = [
 	config.htmlEncodeOutput = false;
 	config.entities = false;
 	config.basicEntities = false;
+	
 	// szövegeket, img tageket nem csomagol automatikusan <p> tagekbe
 	config.autoParagraph = false;
+	
 	// engedélyezi az üres tageket
 	config.fillEmptyBlocks = false;
+	
 	// nem távolítja el a <i ... </i> közé zárt tartalmat - font ikon megjelenítés
 	config.protectedSource.push(/<i[^>]*><\/i>/g);
+	
 	// nem jeleníti meg egyáltalán a {} közötti tartalmat pl.: {$slider}
 	config.protectedSource.push(/{[\s\S]*?}/g); 
+	
 	// nem távolítja el az üres span tag-eket
 	CKEDITOR.dtd.$removeEmpty['span'] = false;
-
-	
 };
