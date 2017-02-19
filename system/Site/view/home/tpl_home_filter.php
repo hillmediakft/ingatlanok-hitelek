3a3e0e60f79533a551fc377ba0edbec7c85c21f2
varnagy-zsolt <varnagyzsolt1975@gmail.com>
2017-02-19 14:48:36 +0100

    javitas

--- system/Site/view/home/tpl_home_filter.php
+++ system/Site/view/home/tpl_home_filter.php
@@ -79,90 +79,62 @@ use System\Libs\Language as Lang;
 			<!-- ALSÓ SOR -->
 			<div class="row">
 				
-                <!-- ALAPTERÜLET -->
+                <!-- ALAPTERÜLET MINIMUM -->
                 <div class="col-sm-2">
                     <?php 
                         $min_terulet = $this->request->has_query('min_alapterulet') ? $this->request->get_query('min_alapterulet') : "";
-                        $max_terulet = $this->request->has_query('max_alapterulet') ? $this->request->get_query('max_alapterulet') : "";
                     ?>
-                    
-				<!-- ALAPTERÜLET SLIDER -->
-<!-- 
-                    <div id="terulet_slider_wrapper" data-min="<?php //echo $min_terulet; ?>" data-max="<?php //echo $max_terulet; ?>">
-                        <span class="item-label"><?php //echo Lang::get('kereso_alapterulet'); ?></span>
-                        <div class="range-wrap">
-                            <div class="range-fields">
-                                <input type="text" id="min_terulet" name="min_alapterulet"/>
-                                <span class="delimiter"></span>
-                                <input type="text" id="max_terulet" name="max_alapterulet"/>
-                            </div>
-                            <div id="terulet_slider" class="slider"></div>
-                            <div class="scale">
-                                <span class="min-value"></span>
-                                <span class="max-value"></span>
-                            </div>
-                        </div>
-                    </div>
--->
                     <span class="item-label"><?php echo Lang::get('kereso_alapterulet'); ?></span>
                     <div class="form-group">
                         <div class="input-group">
-                            <input name="min_alapterulet" type="text" class="form-control" placeholder="minimum">
+                            <input name="min_alapterulet" type="text" class="form-control" placeholder="minimum" value="<?php echo $min_terulet; ?>">
                             <div class="input-group-addon">m<sup>2</sup></div>
                         </div>
                     </div>
                 </div>
 
+                <!-- ALAPTERÜLET MAXIMUM -->
                 <div class="col-sm-2">
+                    <?php 
+                        $max_terulet = $this->request->has_query('max_alapterulet') ? $this->request->get_query('max_alapterulet') : "";
+                    ?>                
                     <span class="item-label">&nbsp;</span>
                     <div class="form-group">
                         <div class="input-group">
-                            <input name="max_alapterulet" type="text" class="form-control" placeholder="maximum">
+                            <input name="max_alapterulet" type="text" class="form-control" placeholder="maximum" value="<?php echo $max_terulet; ?>">
                             <div class="input-group-addon">m<sup>2</sup></div>
                         </div>
                     </div>                    
                 </div>
 
-
-                <!-- ÁR SLIDER -->
+                <!-- ÁR MINIMUM -->
                 <div class="col-sm-2">
-                    
-				<!-- ÁR SLIDER -->
-<!-- 
-                    <div id="ar_slider_wrapper">
-                        <span class="item-label">Ár</span>
-                        <div class="range-wrap">
-                            <div class="range-fields">
-                                <input type="text" id="min_ar" name="min_ar"/>
-                                <span class="delimiter"></span>
-                                <input type="text" id="max_ar" name="max_ar"/>
-                            </div>
-                            <div id="ar_slider" class="slider"></div>
-                            <div class="scale">
-                                <span class="min-value"></span>
-                                <span class="max-value"></span>
-                            </div>
-                        </div>
-                    </div>
--->
-                   <span class="item-label"><?php echo Lang::get('kereso_min_ar'); ?></span>
+                    <?php 
+                        $min_ar = $this->request->has_query('min_ar') ? $this->request->get_query('min_ar') : "";
+                    ?>               
+                    <span class="item-label"><?php echo Lang::get('kereso_min_ar'); ?></span>
                     <div class="form-group">
                         <div class="input-group">
-                            <input name="min_ar" type="text" class="form-control" placeholder="minimum">
+                            <input name="min_ar" type="text" class="form-control" placeholder="minimum" value="<?php echo $min_ar; ?>">
                             <div class="input-group-addon">Ft</div>
                         </div>
                     </div>
-
-				</div>
+                </div>
+                
+                <!-- ÁR MAXIMUM -->
                 <div class="col-sm-2">
+                    <?php 
+                        $max_ar = $this->request->has_query('max_ar') ? $this->request->get_query('max_ar') : "";
+                    ?>                
                     <span class="item-label"><?php echo Lang::get('kereso_max_ar'); ?></span>
                     <div class="form-group">
                         <div class="input-group">
-                            <input name="max_ar" type="text" class="form-control" placeholder="maximum">
+                            <input name="max_ar" type="text" class="form-control" placeholder="maximum" value="<?php echo $max_ar; ?>">
                             <div class="input-group-addon">Ft</div>
                         </div>
                     </div>                    
                 </div>				
+
 
 				<div class="col-sm-4">
 					<button class="find-now-btn"><?php echo Lang::get('kereso_kereses'); ?></button>
