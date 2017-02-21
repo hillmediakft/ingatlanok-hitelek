40038bdd92855f7b63808325919e938664660581
varnagy-zsolt <varnagyzsolt1975@gmail.com>
2017-02-21 15:50:09 +0100

    modositas

--- system/Site/view/ingatlanok/tpl_ingatlanok_filter.php
+++ system/Site/view/ingatlanok/tpl_ingatlanok_filter.php
@@ -1,4 +1,6 @@
-<?php use System\Libs\Language as Lang; ?>
+<?php
+use System\Libs\Language as Lang;
+?>
 <div class="widget main-filter-widget gray-bg">
     <div class="widget-entry">
         <div class="filter-label-block">
@@ -9,19 +11,11 @@
             <!-- ELADÓ/KIADÓ -->
             <div class="row">
                 <div class="col-sm-12">
-                    <?php 
-                        $selected_elado = 'selected';
-                        $selected_kiado = '';
-                        if ($this->request->has_query('tipus')) {
-                            $selected_elado = ($this->request->get_query('tipus') == 1) ? 'selected' : '';
-                            $selected_kiado = ($this->request->get_query('tipus') == 2) ? 'selected' : '';
-                        }
-                    ?>
                     <span class="item-label"><?php echo Lang::get('kereso_elado'); ?>/<?php echo Lang::get('kereso_kiado'); ?></span>
                     <div id="tipus_select_div" class="ui-front">
                         <select name="tipus" id="tipus_select" data-icon="false" class="select filter-select">
-                            <option <?php echo $selected_elado; ?> value="1"><?php echo Lang::get('kereso_elado'); ?></option>
-                            <option <?php echo $selected_kiado; ?> value="2"><?php echo Lang::get('kereso_kiado'); ?></option>
+                            <option <?php echo (isset($filter_params['tipus']) && $filter_params['tipus'] == 1) ? 'selected' : ''; ?> value="1"><?php echo Lang::get('kereso_elado'); ?></option>
+                            <option <?php echo (isset($filter_params['tipus']) && $filter_params['tipus'] == 2) ? 'selected' : ''; ?> value="2"><?php echo Lang::get('kereso_kiado'); ?></option>
                         </select>
                     </div>
                 </div>
@@ -54,34 +48,24 @@
                 </div>
             </div>
  -->            
-            
-
-
-<div class="row">
-    <div class="col-sm-12">
-        <span class="item-label"><?php echo Lang::get('kereso_kerulet'); ?></span>
-        <div id="district_select_div">
-            <select name="kerulet[]" id="district_select" class="selectpicker" data-selected-text-format="count" multiple disabled="disabled" title="-- <?php echo Lang::get('kereso_mindegy'); ?> --">
-                <!-- <option value="">-- <?php //echo Lang::get('kereso_mindegy'); ?> --</option> -->
-                <?php echo $district_list; ?>
-            </select>
-        </div>
-    </div>
-</div>
-
-
-
-
-
-
-
+            <div class="row">
+                <div class="col-sm-12">
+                    <span class="item-label"><?php echo Lang::get('kereso_kerulet'); ?></span>
+                    <div id="district_select_div">
+                        <select name="kerulet[]" id="district_select" class="selectpicker" data-selected-text-format="count" multiple disabled="disabled" title="-- <?php echo Lang::get('kereso_mindegy'); ?> --">
+                            <!-- <option value="">-- <?php //echo Lang::get('kereso_mindegy'); ?> --</option> -->
+                            <?php echo $district_list; ?>
+                        </select>
+                    </div>
+                </div>
+            </div>
 
             <!-- KATEGÓRIA -->
             <div class="row">
                 <div class="col-sm-12">
                     <?php 
-                        $selected_kategoria = ($this->request->has_query('kategoria')) ? (int)$this->request->get_query('kategoria') : '';
-                    ?>
+                        $selected_kategoria = isset($filter_params['kategoria']) ? $filter_params['kategoria'][0]['kat_id'] : '';
+                    ?>                    
                     <span class="item-label"><?php echo Lang::get('kereso_kategoria'); ?></span>
                     <div id="category_select_div" class="ui-front">
                         <select name="kategoria" id="category_select" data-icon="false" class="select filter-select">
@@ -97,13 +81,10 @@
             <!-- ALAPTERÜLET MINIMUM -->
             <div class="row">
                 <div class="col-sm-12">
-                    <?php
-                        $min_terulet = $this->request->has_query('min_alapterulet') ? $this->request->get_query('min_alapterulet') : "";
-                    ?>
                     <span class="item-label"><?php echo Lang::get('kereso_alapterulet'); ?></span>
                     <div class="form-group">
                         <div class="input-group">
-                            <input name="min_alapterulet" type="text" class="form-control" placeholder="minimum" value="<?php echo $min_terulet; ?>">
+                            <input name="min_alapterulet" type="text" class="form-control" placeholder="minimum" value="<?php echo (isset($filter_params['min_alapterulet'])) ? $filter_params['min_alapterulet'] : ''; ?>">
                             <div class="input-group-addon">m<sup>2</sup></div>
                         </div>
                     </div>
@@ -113,13 +94,10 @@
             <!-- ALAPTERÜLET MAXIMUM -->
             <div class="row">
                 <div class="col-sm-12">
-                    <?php 
-                        $max_terulet = $this->request->has_query('max_alapterulet') ? $this->request->get_query('max_alapterulet') : "";
-                    ?>
                     <span class="item-label">&nbsp;</span>
                     <div class="form-group">
                         <div class="input-group">
-                            <input name="max_alapterulet" type="text" class="form-control" placeholder="maximum" value="<?php echo $max_terulet; ?>">
+                            <input name="max_alapterulet" type="text" class="form-control" placeholder="maximum" value="<?php echo (isset($filter_params['max_alapterulet'])) ? $filter_params['max_alapterulet'] : ''; ?>">
                             <div class="input-group-addon">m<sup>2</sup></div>
                         </div>
                     </div> 
@@ -130,13 +108,10 @@
             <!-- ÁR MINIMUM -->
             <div class="row">
                 <div class="col-sm-12">
-                    <?php 
-                        $min_ar = $this->request->has_query('min_ar') ? $this->request->get_query('min_ar') : "";
-                    ?>
                     <span class="item-label"><?php echo Lang::get('kereso_min_ar'); ?></span>
                     <div class="form-group">
                         <div class="input-group">
-                            <input name="min_ar" type="text" class="form-control" placeholder="minimum" value="<?php echo $min_ar; ?>">
+                            <input name="min_ar" type="text" class="form-control" placeholder="minimum" value="<?php echo (isset($filter_params['min_ar'])) ? $filter_params['min_ar'] : ''; ?>">
                             <div class="input-group-addon" id="ar_min_mertek">M Ft</div>
                         </div>
                     </div>
@@ -146,13 +121,10 @@
             <!-- ÁR MAXIMUM -->
             <div class="row">
                 <div class="col-sm-12">
-                    <?php 
-                        $max_ar = $this->request->has_query('max_ar') ? $this->request->get_query('max_ar') : "";
-                    ?>
                     <span class="item-label"><?php echo Lang::get('kereso_max_ar'); ?></span>
                     <div class="form-group">
                         <div class="input-group">
-                            <input name="max_ar" type="text" class="form-control" placeholder="maximum" value="<?php echo $max_ar; ?>">
+                            <input name="max_ar" type="text" class="form-control" placeholder="maximum" value="<?php echo (isset($filter_params['max_ar'])) ? $filter_params['max_ar'] : ''; ?>">
                             <div class="input-group-addon" id="ar_max_mertek">M Ft</div>
                         </div>
                     </div> 
@@ -171,20 +143,3 @@
         </form>
     </div>
 </div>
-
-
-
-
-
-<!-- 
-
-<span class="item-label">Property Type</span>
-<div id="select-type-holder3" class="ui-front">
-    <select name="prop-select" data-icon="false" class="select filter-select">
-        <option selected="selected">Any</option>
-        <option>1</option>
-        <option>2</option>
-        <option>3</option>
-    </select>
-</div>
- -->
