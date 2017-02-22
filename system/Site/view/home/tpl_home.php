975e7794aca7c90d21fb97a957f07331f7119458
varnagy-zsolt <varnagyzsolt1975@gmail.com>
2017-02-22 13:10:51 +0100

    kiemelet ingatlanoknal eledo/kiad cimke a dobozra

--- system/Site/view/home/tpl_home.php
+++ system/Site/view/home/tpl_home.php
@@ -113,6 +113,19 @@ use System\Libs\Language as Lang;
                                 <li>
                                     <div class="item">
                                         <div class="preview">
+
+                                            <!-- eladó/kiadó cimke-->                                        
+                                            <?php
+                                                if ($value['tipus'] == 1) {
+                                                    $label = Lang::get('kereso_elado');
+                                                    $css_class = 'sale';
+                                                } else {
+                                                    $label = Lang::get('kereso_kiado');
+                                                    $css_class = 'rest';
+                                                }
+                                            ?>
+                                            <span class="item-label <?php echo $css_class; ?>"><?php echo $label; ?></span>
+
                                             <?php if ($value['kepek']) { ?>
                                                 <a href="ingatlanok/adatlap/<?php echo $value['id'] . '/' . $this->str_helper->stringToSlug($value['ingatlan_nev_' . LANG]); ?>">
                                                     <img src="<?php echo $this->url_helper->thumbPath(Config::get('ingatlan_photo.upload_path') . $photo_array[0], false, 'small'); ?>" alt="<?php echo $value['ingatlan_nev_' . LANG]; ?>">
