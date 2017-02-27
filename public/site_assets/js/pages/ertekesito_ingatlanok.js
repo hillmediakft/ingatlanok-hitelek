63901865af4d4d433c479abd77232bc025085c28
varnagy-zsolt <varnagyzsolt1975@gmail.com>
2017-02-27 21:52:23 +0100

    contactAgent metodus, es toastr config

--- public/site_assets/js/pages/ertekesito_ingatlanok.js
+++ public/site_assets/js/pages/ertekesito_ingatlanok.js
@@ -1,5 +1,20 @@
 var Ertekesito_ingatlanok = function () {
 
+    toastr.options = {
+        "closeButton": true,
+        "debug": false,
+        "positionClass": "toast-top-right",
+        "onclick": null,
+        "showDuration": "1000",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+
     var equalHeights = function () {
         console.log('asf');
         setTimeout(function () {
@@ -7,10 +22,38 @@ var Ertekesito_ingatlanok = function () {
         }, 200);
     };
 
+    // kapcsolat űrlap az irodánk oldalon
+    var contactAgent = function () {
+
+        $("#contact-form-agent").on('submit', function (event) {
+            event.preventDefault();
+            var $form = $(this);
+
+            $.ajax({
+                type: $form.attr('method'),
+                url: $form.attr('action'),
+                dataType: 'json',
+                data: $form.serialize(),
+                beforeSend: function() {
+                    $('#submit-button').addClass('button-loading');
+                },                
+                success: function (result) {
+                    //result = JSON.parse(result);
+                    $('#submit-button').removeClass('button-loading');
+                    document.getElementById("contact-form-agent").reset();
+                    toastr[result.status](result.message, result.title)
+                },
+            });
+
+        });
+    }
+
+
     return {
         //main function to initiate the module
         init: function () {
             equalHeights();
+            contactAgent();
         }
     };
 }();
