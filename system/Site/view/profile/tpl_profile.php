8e6124a74d51a8548c634a1e68d36873c5d3579a
varnagy-zsolt <varnagyzsolt1975@gmail.com>
2017-02-28 16:28:59 +0100

    site - profil oldal template letrehozva

---
+++ system/Site/view/profile/tpl_profile.php
@@ -0,0 +1,33 @@
+<?php use System\Libs\Language as Lang; ?>
+<div id="content" class="container-fluid">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="breadcrumbs">
+                    <span class="clickable"><a href="<?php echo $this->request->get_uri('site_url');?>"><?php echo Lang::get('menu_home'); ?></a></span>
+                    <span class="delimiter">/</span>
+                    <span class="active-page"><?php echo Lang::get('menu_profil'); ?></span>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-sm-12">
+                <div class="empty-space"></div>
+            </div>
+        </div>
+
+
+        <div class="row">
+            <div class="col-sm-12">
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </div>
+        </div>
+        
+    </div>
+</div>
