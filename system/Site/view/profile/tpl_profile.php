ff6e67d54c58e28e57b69994eac74f24d9e63e6a
varnagy-zsolt <varnagyzsolt1975@gmail.com>
2017-03-01 14:56:16 +0100

    modositas

--- system/Site/view/profile/tpl_profile.php
+++ system/Site/view/profile/tpl_profile.php
@@ -1,4 +1,7 @@
-<?php use System\Libs\Language as Lang; ?>
+<?php
+use System\Libs\Config;
+use System\Libs\Language as Lang;
+?>
 <div id="content" class="container-fluid">
     <div class="container">
         <div class="row">
@@ -6,7 +9,7 @@
                 <div class="breadcrumbs">
                     <span class="clickable"><a href="<?php echo $this->request->get_uri('site_url');?>"><?php echo Lang::get('menu_home'); ?></a></span>
                     <span class="delimiter">/</span>
-                    <span class="active-page"><?php echo Lang::get('menu_profil'); ?></span>
+                    <span class="active-page"><?php echo Lang::get('header_top_profil'); ?></span>
                 </div>
             </div>
         </div>
@@ -17,17 +20,165 @@
             </div>
         </div>
 
-
         <div class="row">
-            <div class="col-sm-12">
-Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            <div class="col-md-9 col-sm-8">
+
+
+
+
+
+                            <div class="row">                    
+                            <h1><i class="fa fa-user"></i> Profil adatok szerkesztése</h1>
+                                <div class="col-sm-6">
+                                    <div class="contacts-block">
+                                        <div class="contact-form">
+
+                                            <form action="#">
+                                                
+                                                <input type="password" name="password" class="name" placeholder="Password">
+                                                <input type="password" name="password_again" class="name" placeholder="Password again">
+                                                <input type="password" name="password_new" class="name" placeholder="New password">
+
+                                                <button class="send-btn style-2">Send</button>
+                                            </form>
+                                           
+                                        </div>
+                                    </div>        
+                                </div>
+                                <div class="col-sm-6">
+                                    <div class="contacts-block">
+                                        <div class="contact-form">
+
+                                            <form action="#">
+                                                
+                                                <input type="text" class="name" placeholder="Name">
+                                                <input type="email" class="email" placeholder="Email">
+                                                <button class="send-btn style-2">Send Message</button>
+                                            </form> 
+                                           
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+
+
+                <div class="objects-block list-sidebar">
+
+                    <?php if (count($properties) > 0) : ?> 
+
+                    <h1><i class="fa fa-line-chart"></i> Árváltozás értesítésre jelölt ingatlanok</h1>
+                        <div class="notification-box info">
+                            <div class="icon"><i class="fa fa-info"></i></div>
+                            <div class="descr"><span class="message">Ingatlan eltávolításához kattintson a kuka ikonra!</span></div>
+                        </div>                         
+                        <?php
+                        foreach ($properties as $value) {
+                            $photo_array = json_decode($value['kepek']);
+                            ?>
+                            <div class="row" id="followed_item_<?php echo $value['id']; ?>">
+                                <div class="col-sm-12">
+                                    <div class="item">
+                                        <div class="preview">
+                                            <?php $this->html_helper->showLowerPriceIcon($value);?>
+                                            <!-- eladó/kiadó cimke-->                                        
+                                            <?php
+                                            if ($value['tipus'] == 1) {
+                                                $label = Lang::get('kereso_elado');
+                                                $css_class = 'sale';
+                                            } else {
+                                                $label = Lang::get('kereso_kiado');
+                                                $css_class = 'rest';
+                                            }
+                                            ?>
+                                            <span class="item-label <?php echo $css_class; ?>"><?php echo $label; ?></span>
+
+                                            <a href="<?php echo $this->request->get_uri('site_url') . Config::get('url.ingatlanok.adatlap.' . LANG) . '/' . $value['id'] . '/' . $this->str_helper->stringToSlug($value['ingatlan_nev_' . LANG]); ?>">
+                                            <?php if (!is_null($value['kepek'])) { ?>
+                                                <img src="<?php echo $this->url_helper->thumbPath(Config::get('ingatlan_photo.upload_path') . $photo_array[0], false, 'small'); ?>" alt="<?php echo $value['ingatlan_nev_' . LANG]; ?>">
+                                            <?php } else { ?>
+                                                <img src="<?php echo Config::get('ingatlan_photo.upload_path') . 'placeholder.jpg'; ?>" alt="<?php echo $value['ingatlan_nev_' . LANG]; ?>">
+                                            <?php } ?>
+                                            </a>                                        
+
+                                        </div>
+                                        <div class="thumbnail-container">
+                                            <div class="item-thumbnail">
+                                                <div class="single-thumbnail">
+                                                    <span class="value"><?php echo $value['kat_nev_' . LANG]; ?></span>
+                                                </div>
+                                                <div class="single-thumbnail">
+                                                    <span class="value"><?php echo $value['szobaszam']; ?> szoba</span>
+                                                </div>
+                                                <div class="single-thumbnail">
+                                                    <span class="value"><?php echo $value['alapterulet']; ?> m<sup>2</sup></span>
+                                                </div>
+                                            </div>
+                                            <div class="item-info">
+                                                <span class="price"> 
+                                                    <?php $this->html_helper->showPrice($value);?>
+                                                </span>
+                                                <div class="pull-right buttons">
+                                                    <a href="javascript:void();" id="delete_followed_<?php echo $value['id']; ?>" data-id="<?php echo $value['id']; ?>" class="share"><i class="fa fa-trash-o fa-2x"></i></a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="item-entry">
+                                            <span class="item-title"><a href="<?php echo $this->request->get_uri('site_url') . Config::get('url.ingatlanok.adatlap.' . LANG) . '/' . $value['id'] . '/' . $this->str_helper->stringToSlug($value['ingatlan_nev_' . LANG]); ?>"><?php echo $value['ingatlan_nev_' . LANG]; ?></a></span>
+                                            <p class="item-text"><p><?php
+                                                echo $value['city_name'];
+                                                echo (isset($value['kerulet'])) ? ', ' . $value['kerulet'] . ' kerület' : '';
+                                                ?></p></p>
+                                        </div>
+                                    </div>
+                                </div>                   
+                            </div>
+                        <?php } ?>
+
+                    <?php endif ?>
+
+                    <!-- INFO BOX - HA NINCSENEK LISTAELEMEK -->
+                    <div class="notification-box caution" <?php echo (count($properties) > 0) ? 'style="display: none;"' : ''; ?>>
+                        <div class="icon"><i class="fa fa-info"></i></div>
+                        <div class="descr"><span class="message">Jelenleg egyetlen ingatlan árváltozásáról sem kért értesítést!</span></div>
+                    </div>
+
+
+                    <div class="row">
+                        <div class="col-sm-12">
+                            <div class="empty-space"></div>
+                        </div>
+                    </div>
+                </div>
             </div>
+
+            <!-- SIDEBAR -->
+            <div class="col-md-3 col-sm-4">
+                <aside class="sidebar main-sidebar" style="padding-top: 0;">
+                    <!-- KERESŐ DOBOZ -->
+                    <?php //include($this->path('tpl_ingatlanok_filter')); ?>
+                    <!-- REFERENSEK DOBOZ -->
+                    <?php //include($this->path('tpl_modul_referenscontact')); ?>
+                    <!-- KIEMELT INGATLANOK DOBOZ -->
+                    <?php //include($this->path('tpl_modul_kiemeltingatlanok')); ?>
+                    <!-- KIEMELT INGATLANOK DOBOZ -->
+                    <?php //include($this->path('tpl_modul_banner')); ?>
+                </aside>        
+            </div> <!-- SIDEBAR END -->
+
         </div>
+
+
+
+
+
+
+
+
+
+
+
+
         
     </div>
 </div>
