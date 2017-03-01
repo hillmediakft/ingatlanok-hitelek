f70b44b67f6c2c2eefa2db817ce91ecfc1105797
varnagy-zsolt <varnagyzsolt1975@gmail.com>
2017-03-01 14:52:56 +0100

    deleteFollowed metodus hozzaadva, es modositasok

--- system/Site/Controller/Profile.php
+++ system/Site/Controller/Profile.php
@@ -14,6 +14,7 @@ class Profile extends SiteController {
 	{
 		parent::__construct();
 		$this->loadModel('user_model');
+		$this->loadModel('ingatlanok_model');
 	}
 
 	/**
@@ -28,14 +29,58 @@ class Profile extends SiteController {
         $data['description'] = $page_data['metadescription_' . $this->lang];
         $data['keywords'] = $page_data['metakeywords_' . $this->lang];	
 
+        $id = (int)Auth::getUser('id');
+        // ha nincs bejelentkezve
+        if (is_null($id)) {
+        	$this->response->redirect('error');
+        }
+
+        $data['user'] = $this->user_model->selectUser($id);
+
+        $data['properties'] = $this->ingatlanok_model->followedByProperty($id);
 
         $view = new View();
-        $view->setHelper(array('url_helper', 'str_helper'));
+        $view->setHelper(array('url_helper', 'str_helper', 'num_helper', 'html_helper'));
 
 //$this->view->debug(true); 
         //$view->add_links(array('bootstrap-select'));
-        //$view->add_link('js', SITE_JS . 'pages/profile.js');
+        $view->add_link('js', SITE_JS . 'pages/profile.js');
         $view->render('profile/tpl_profile', $data);        	
+	}
+
+	/**
+	 * Követett ingatlan törlése
+	 */
+	public function deleteFollowed()
+	{
+		if ($this->request->is_ajax()) {
+			$property_id = $this->request->get_post('ingatlan_id', 'integer');
+			$user_id = (int)Auth::getUser('id');
+
+			if (is_null($user_id)) {
+				$this->response->json(array(
+					'status' => 'error',
+					'message' => 'Nincs bejelentkezve a felhasználó!'
+					));	
+			}
+
+			$result = $this->ingatlanok_model->deleteFollowed($property_id, $user_id);
+
+			if ($result === 1) {
+				$this->response->json(array(
+					'status' => 'success',
+					'message' => ''
+					));
+			} else {
+				$this->response->json(array(
+					'status' => 'error',
+					'message' => 'Adatbázis lekérdezési hiba!'
+					));				
+			}
+
+		} else {
+			$this->response->redirect('error');
+		}
 	}
 
 }
