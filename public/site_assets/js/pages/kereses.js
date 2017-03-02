4d4e404ba287c73e3e953d7d4fac1ab33bac8966
varnagy-zsolt <varnagyzsolt1975@gmail.com>
2017-03-02 16:09:08 +0100

    file hozzaadva (egyelore nincs hasznalatban)

---
+++ public/site_assets/js/pages/kereses.js
@@ -0,0 +1,16 @@
+var Kereses = function () {
+ 
+
+	return {
+ 
+		//main method to initiate page
+		init: function () {           
+
+		}
+
+	};
+}();
+
+$(document).ready(function() {
+	Kereses.init();
+});
