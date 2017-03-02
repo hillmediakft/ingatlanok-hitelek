b21f0f181b76ee0733546326759d83bb2f2d4c97
varnagy-zsolt <varnagyzsolt1975@gmail.com>
2017-03-02 16:06:29 +0100

    changePassword(), changeUserdata() metodusok hozzaadva

--- system/Site/Controller/Profile.php
+++ system/Site/Controller/Profile.php
@@ -4,6 +4,8 @@ namespace System\Site\Controller;
 use System\Core\SiteController;
 use System\Core\View;
 use System\Libs\Config;
+use System\Libs\Session;
+use System\Libs\Message;
 use System\Libs\Auth;
 use System\Libs\DI;
 use System\Libs\Validate;
@@ -43,7 +45,7 @@ class Profile extends SiteController {
         $view->setHelper(array('url_helper', 'str_helper', 'num_helper', 'html_helper'));
 
 //$this->view->debug(true); 
-        //$view->add_links(array('bootstrap-select'));
+        $view->add_links(array('validation'));
         $view->add_link('js', SITE_JS . 'pages/profile.js');
         $view->render('profile/tpl_profile', $data);        	
 	}
@@ -81,6 +83,132 @@ class Profile extends SiteController {
 		} else {
 			$this->response->redirect('error');
 		}
+	}
+
+	/**
+	 * Jelszó megváltoztatása
+	 */
+	public function changePassword()
+	{
+		if ($this->request->is_ajax()) {
+
+			$password_new = $this->request->get_post('password_new');
+			$password_new_again = $this->request->get_post('password_new_again');
+			$password_old = $this->request->get_post('password_old');
+
+			if (empty($password_new) || empty($password_new_again) || empty($password_old)) {
+				$this->response->json(array(
+					'status' => 'error',
+					'message' => 'Minden jelszó mezőt ki kell töltenie!'
+					));
+		    }
+		    if ($password_new != $password_new_again) {
+				$this->response->json(array(
+					'status' => 'error',
+					'message' => 'A két új jelszónak meg kell egyeznie!'
+					));
+		    }
+
+
+			//bejelentkezett user id-je
+			$user_id = Auth::getUser('id');
+			// user adatainak lekérdezése
+			$user_data = $this->user_model->selectUser($user_id);
+
+			// jelszó kompatibilitás library betöltése régebbi php verzió esetén
+			$this->user_model->load_password_compatibility();
+			// crypt the user's password with the PHP 5.5's password_hash() function, results in a 60 character
+			// hash string. the PASSWORD_DEFAULT constant is defined by the PHP 5.5, or if you are using PHP 5.3/5.4,
+			// by the password hashing compatibility library. the third parameter looks a little bit shitty, but that's
+			// how those PHP 5.5 functions want the parameter: as an array with, currently only used with 'cost' => XX
+			$hash_cost_factor = (Config::get('hash_cost_factor') !== null) ? Config::get('hash_cost_factor') : null;
+
+
+			// régi jelszó tesztelése
+			if (!password_verify($password_old, $user_data['password_hash'])) {
+				$this->response->json(array(
+					'status' => 'error',
+					'message' => 'A régi jelszó nem helyes!'
+					));
+			}
+
+			// új jelszó titkosítva
+			$new_password_hash = password_hash($password_new, PASSWORD_DEFAULT, array('cost' => $hash_cost_factor));
+
+			// új password hash adatbázisba írása
+			$result = $this->user_model->update($user_id, array('password_hash' => $new_password_hash));
+
+			if ($result !== false) {
+				$this->response->json(array(
+					'status' => 'success',
+					'message' => 'Új jelszó mentése sikeres!'
+					));
+			} else {
+				$this->response->json(array(
+					'status' => 'error',
+					'message' => Message::show('unknown_error')
+					));				
+			}
+
+		} else {
+			$this->response->redirect('error');
+		}
+		
+	}
+
+	/**
+	 * Felhasználói név vagy email cím megváltoztatása
+	 */
+	public function changeUserdata()
+	{
+		if ($this->request->is_ajax()) {
+			$new_name = $this->request->get_post('name');
+			$new_email = $this->request->get_post('email');
+			
+			//bejelentkezett user id-je
+			$user_id = Auth::getUser('id');
+			//Megvizsgáljuk, hogy van-e már ilyen nevű user, de nem az amit módosítani akarunk
+			$result_name = $this->user_model->checkUserNoLoggedIn($user_id, $new_name);
+			
+			if ($result_name) {
+				$this->response->json(array(
+					'status' => 'error',
+					'message' => 'Már létezik ilyen felhasználó név!'
+					));
+			}
+
+			//Megvizsgáljuk, hogy van-e már ilyen email cím (de nem az amit módosítani akarunk)
+			$result_email = $this->user_model->checkEmailNoLoggedIn($user_id, $new_email);
+			if ($result_email) {
+				$this->response->json(array(
+					'status' => 'error',
+					'message' => 'Már létezik ilyen e-mail cím!'
+					));
+			}
+
+			// update rekord
+			$update = $this->user_model->update($user_id, array(
+				'name' => $new_name,
+				'email' => $new_email
+				));
+
+			if ($update !== false) {
+
+		        Session::set('user_data.name', $new_name);				
+		        Session::set('user_data.email', $new_email);				
+
+				$this->response->json(array(
+					'status' => 'success',
+					'message' => 'A felhasználó adatai módosítva!',
+					'new_name' => $new_name
+					));
+			}
+
+
+		} else {
+			$this->response->redirect('error');
+		}
+		
 	}
 
 }
