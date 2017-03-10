515b62b7309c2158b52da8489838e3689d5e13b6
varnagy-zsolt <varnagyzsolt1975@gmail.com>
2017-03-10 13:16:43 +0100

    savesearch() metodus hozzaadva

--- public/site_assets/js/pages/ingatlanok.js
+++ public/site_assets/js/pages/ingatlanok.js
@@ -1,5 +1,82 @@
 var Ingatlanok = function () {
 
+    toastr.options = {
+        "closeButton": true,
+        "debug": false,
+        "positionClass": "toast-top-right",
+        "onclick": null,
+        "showDuration": "1000",
+        "hideDuration": "1000",
+        "timeOut": "5000",
+        "extendedTimeOut": "1000",
+        "showEasing": "swing",
+        "hideEasing": "linear",
+        "showMethod": "fadeIn",
+        "hideMethod": "fadeOut"
+    }
+
+    /**
+     * Keresés mentése
+     */
+    var saveSearch = function () {
+
+        $('#save_search').on('click', function(event){
+            event.preventDefault();
+            
+            var gomb = $(this);
+
+            // ha már van disabled osztálya a gombnak
+            if ($(gomb).hasClass('disabled')) {
+                return false;
+            }
+
+            // url
+            var searchurl = window.location.href;
+
+            $.ajax({
+                url: '/kereses/savesearch',
+                type: 'post',
+                dataType: 'json',
+                data: {url: searchurl},
+            
+                beforeSend: function() {
+                    /*
+                    App.blockUI({
+                        boxed: true,
+                        message: 'Feldolgozás...'
+                    });
+                    */
+
+                },
+                complete: function(){
+                    /*
+                    App.unblockUI();
+                    */
+                },
+                // itt kapjuk meg (és dolgozzuk fel) a feldolgozó php által visszadott adatot 
+                success: function(result){
+                    if(result.status == 'success'){
+                        // disabled-re állítjuk a gombot
+                        $(gomb).addClass('disabled');
+                        // üzenet megjelenítése
+                        toastr['success'](result.message);
+                    }
+            
+                    if(result.status == 'error'){
+                        // üzenet megjelenítése
+                        toastr['error'](result.message);
+                    }
+            
+                },
+                error: function(result, status, e){
+                    console.log(e);
+                } 
+            });
+
+        });
+
+    }; 
+
     var equalHeights = function () {
         setTimeout(function () {
             $('#equalheight-property-list div.item').equalHeights();
@@ -9,6 +86,7 @@ var Ingatlanok = function () {
     return {
         //main function to initiate the module
         init: function () {
+            saveSearch();
             equalHeights();
         }
     };
