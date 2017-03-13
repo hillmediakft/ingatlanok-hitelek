73be1112a8ac8de2992fef91509a9b9ff51ad7cd
varnagy-zsolt <varnagyzsolt1975@gmail.com>
2017-03-13 18:36:04 +0100

    file letrehozva

---
+++ system/Site/view/_template/tpl_modul_banner_befektetoknek.php
@@ -0,0 +1,14 @@
+<?php 
+use System\Libs\Config;
+use System\Libs\Language as Lang;
+?>
+<div class="widget banner">
+    <div class="banner-img">
+        <img src="<?php echo SITE_IMAGE; ?>befektetoknek_banner.jpg" alt="">
+    </div>
+    <div class="banner-entry">
+        <span class="banner-title"><?php echo Lang::get('home_szolgaltatasok_2_cim'); ?></span>
+        <span class="banner-sub"><?php echo Lang::get('home_szolgaltatasok_2_szoveg'); ?></span>
+        <a href="<?php echo $this->request->get_uri('site_url') . Config::get('url.befektetoknek.index.' . LANG); ?>" class="learn-more">Tovább</a>
+    </div>
+</div>
---
+++ system/Site/view/_template/tpl_modul_banner_mennyiterazingatlanom.php
@@ -0,0 +1,14 @@
+<?php 
+use System\Libs\Config;
+use System\Libs\Language as Lang;
+?>
+<div class="widget banner">
+    <div class="banner-img">
+        <img src="<?php echo SITE_IMAGE; ?>mennyit-er-az-ingatlanom-banner.jpg" alt="">
+    </div>
+    <div class="banner-entry">
+        <span class="banner-title"><?php echo Lang::get('home_szolgaltatasok_1_cim'); ?></span>
+        <span class="banner-sub"><?php echo Lang::get('home_szolgaltatasok_1_szoveg'); ?></span>
+        <a href="<?php echo $this->request->get_uri('site_url') . Config::get('url.mennyit-er-az-ingatlanom.index.' . LANG); ?>" class="learn-more">Tovább</a>
+    </div>
+</div>
