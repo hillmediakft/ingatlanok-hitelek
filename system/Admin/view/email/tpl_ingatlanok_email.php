5f8d9ff1d2d3999e9df157da37f78f71b9e27554
varnagy-zsolt <varnagyzsolt1975@gmail.com>
2017-03-26 14:41:44 +0200

    file hozzaadva

---
+++ system/Admin/view/email/tpl_ingatlanok_email.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width" />
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Értesítés ingatlanokról</title>
+
+</head>
+<body>
+
+    <h2>Tisztelt érdeklődő!</h2>
+    <p>Az ön által megadott paraméterek alapján az alábbi ingatlanokat tudjuk ajánlani.</p>
+
+    <div>
+        <strong>Ingatlanok:</strong>
+        <br>
+        {html_data}
+    </div>
+
+    <div>
+        Referens neve: {ref_name} 
+    </div>
+    <div>
+        Telefon: {ref_phone} 
+    </div>
+    <div>
+        Email: {ref_email} 
+    </div>
+
+</body>
+</html>
