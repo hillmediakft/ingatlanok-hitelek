c936d88aac79a6276c16d7357e08d2398056c40a
varnagy-zsolt <varnagyzsolt1975@gmail.com>
2017-03-28 13:16:32 +0200

    modositas

--- system/Admin/view/email/tpl_ingatlanok_email.php
+++ system/Admin/view/email/tpl_ingatlanok_email.php
@@ -9,8 +9,8 @@
         <table border="0" cellpadding="0" cellspacing="0" border-collapse="separate" style="mso-table-lspace: 0pt; mso-table-rspace: 0pt; width: 100%; background-color: #f6f6f6; width: 100%;">
             <tr>
                 <td>&nbsp;</td>
-                <td style=" display: block; Margin: 0 auto !important; max-width: 580px; padding: 10px; width: auto !important; width: 580px;">
-                    <div style="box-sizing: border-box; display: block; Margin: 0 auto; max-width: 580px; padding: 10px;">
+                <td style=" display: block; margin: 0 auto !important; max-width: 580px; padding: 10px; width: auto !important; width: 580px;">
+                    <div style="box-sizing: border-box; display: block; margin: 0 auto; max-width: 580px; padding: 10px;">
 
                         <!-- START CENTERED WHITE CONTAINER -->
                         <table style=" background: #fff; border-radius: 3px; width: 100%;">
@@ -27,7 +27,7 @@
                                                     <tbody>
                                                         <tr>
                                                             <td align="left">
-                                                                <table border="0" cellpadding="0" cellspacing="5">
+                                                                <table border="0" cellpadding="0" cellspacing="8">
                                                                     <tbody>
                                                                         {html_data}
                                                                     </tbody>
@@ -48,7 +48,7 @@
 
                         <!-- START FOOTER -->
                         <div style="clear: both; padding-top: 10px; text-align: center; width: 100%;">
-                            <hr style="border: 0; border-bottom: 1px solid #fff; Margin: 10px 0;">
+                            <hr style="border: 0; border-bottom: 1px solid #fff; margin: 10px 0;">
                             <table border="0" cellpadding="0" cellspacing="0" style="width: 100%">
                                 <tr>
                                     <td style="color: #999999; font-size: 12px; text-align: center">
