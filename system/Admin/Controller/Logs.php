f7815bab9d19e659e93c7bb3e0acd50b425f49c8
varnagy-zsolt <varnagyzsolt1975@gmail.com>
2017-04-05 19:11:26 +0200

    ha belepunk a naplozas oldalra, akkor a last_log_id cookie frissul a log tablaban laevo utolso id-re, a last_log_number cookie nullazodik

--- system/Admin/Controller/Logs.php
+++ system/Admin/Controller/Logs.php
@@ -15,6 +15,10 @@ class Logs extends AdminController {
 
     public function index()
     {
+        // last_log_id beállítása
+        $last_log_id = $this->logs_model->lastLogId();
+        Cookie::set('last_log_id', $last_log_id, -1);
+        // last_log_number értéke 0
         Cookie::set('last_log_number', 0, -1);
 
         $data['title'] = 'Naplózás oldal';
