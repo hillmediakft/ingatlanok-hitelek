b5706a084eda0d1cfbff507c686838f89f33d387
varnagy-zsolt <varnagyzsolt1975@gmail.com>
2017-04-05 11:34:12 +0200

    email kuldes az adminoknak valtozaskor

--- system/config/events.php
+++ system/config/events.php
@@ -3,6 +3,7 @@ use System\Libs\LogIntoDb;
 use System\Libs\Emailer;
 use System\Libs\DI;
 use System\Libs\Query;
+use System\Libs\Auth;
  
 $config['events'] = array(
 
@@ -82,7 +83,57 @@ $config['events'] = array(
 
 		}
        		
-	} 
+	},
+	/**
+	 * Email küldés (a beállított elemknél) ingatlan változás esetén
+	 *
+	 * @param array $ref_num_arr  - referencia számokat tartalmazó tömb
+	 * @param string $message 	  - üzenet (pl.: referencia számú ingatlan létrehozva)
+	 */
+	'send_info_email' => function($ref_num_arr, $message){
+
+		$connect = DI::get('connect');	
+		$query = new Query($connect);
+
+		$query->set_table('settings');
+		$query->set_columns('*');
+		$settings = $query->select();
+
+		$agent = Auth::getUser('first_name') . ' ' . Auth::getUser('last_name');
+
+		$from_email = $settings[0]['email'];
+		$from_name = 'Admin rendszer üzenet';
+		$subject = 'Admin rendszer üzenet - értesítés változásról';
+		$to_email = $settings[0]['email'];
+		$to_name = $settings[0]['ceg'];
+
+            $html_data = "";
+            foreach ($ref_num_arr as $key => $ref_num) {
+                $html_data .= "<tr>\r\n";
+                $html_data .= "<td><strong>#" . $ref_num . "</strong></td>";
+                $html_data .= "<td>" . $message . "</td>";
+                $html_data .= "</tr>\r\n";
+            }
+
+            $html_data .= "<tr>\r\n";
+            $html_data .= "<td colspan='2'>&nbsp;</td>\r\n";
+            $html_data .= "</tr>\r\n";
+
+            $html_data .= "<tr>\r\n";
+            $html_data .= "<td colspan='2'><strong>Referens:</strong> " . $agent . "</td>\r\n";
+            $html_data .= "</tr>\r\n";
+
+
+		$template_data = array(
+			'html_data' => $html_data,
+			);
+
+        // paraméterek: ($from_email, $from_name, $to_email, $to_name, $subject, $form_data, $template)
+        $emailer = new Emailer($from_email, $from_name, $to_email, $to_name, $subject, $template_data, 'ertesites_valtozasrol');
+        $emailer->setArea('admin');
+    $emailer->setDebug(true);        
+        $emailer->send();
+	},	 
 
 
 );
