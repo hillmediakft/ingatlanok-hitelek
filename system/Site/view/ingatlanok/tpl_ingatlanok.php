30d39e15ff47f693542d344bfafd730c7bc43738
varnagy-attila <vucuka@gmail.com>
2017-04-23 10:10:25 +0200

    Link javítás

--- system/Site/view/ingatlanok/tpl_ingatlanok.php
+++ system/Site/view/ingatlanok/tpl_ingatlanok.php
@@ -128,7 +128,7 @@ if ($value == 'banner') { ?>
                                         </div>
                                     </div>
                                     <div class="item-entry">
-                                        <span class="item-title"><a href="ingatlanok/adatlap/<?php echo $value['id'] . '/' . $this->str_helper->stringToSlug($value['ingatlan_nev_' . LANG]); ?>"><?php echo $value['ingatlan_nev_' . LANG]; ?></a></span>
+                                        <span class="item-title"><a href="<?php echo $this->request->get_uri('site_url') . Config::get('url.ingatlanok.adatlap.' . LANG) . '/' . $value['id'] . '/' . $this->str_helper->stringToSlug($value['ingatlan_nev_' . LANG]); ?>"><?php echo $value['ingatlan_nev_' . LANG]; ?></a></span>
                                         <p><?php
                                             echo $value['city_name'];
                                             echo (isset($value['kerulet'])) ? ', ' . $value['kerulet'] . '. ' . Lang::get('adatlap_kerulet') : '';
