90a2aec4d3df0ea2bea622508977609103ec48d3
varnagy-attila <vucuka@gmail.com>
2017-04-30 14:00:03 +0200

    Betűtípus cseréje, a Lato nem volt jó

--- system/Site/view/_template/tpl_layout.php
+++ system/Site/view/_template/tpl_layout.php
@@ -14,7 +14,7 @@ use System\Libs\Auth; ?>
         <meta name="keywords" content="<?php echo $keywords; ?>" />
         <base href="<?php echo BASE_URL; ?>">
         <!-- BEGIN GLOBAL MANDATORY STYLES -->
-       <link href="https://fonts.googleapis.com/css?family=Lato:400,700&amp;subset=latin-ext" rel="stylesheet">
+       <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700&amp;subset=latin-ext" rel="stylesheet">
         <!-- BOOTSTRAP CSS v3.3.5 -->
         <link rel="stylesheet" type="text/css" media="screen" href="<?php echo SITE_CSS; ?>bootstrap.min.css">
         <link rel="stylesheet" type="text/css" media="screen" href="<?php echo SITE_ASSETS; ?>fonts/font-awesome-4.4.0/css/font-awesome.min.css">
