41855ae990a5eb767050af8aeede5328f14f4ed1
varnagy-attila <vucuka@gmail.com>
2017-04-30 22:24:25 +0200

    mod

--- system/Site/view/_template/tpl_layout.php
+++ system/Site/view/_template/tpl_layout.php
@@ -32,15 +32,28 @@ use System\Libs\Auth; ?>
         <!-- END PAGE LEVEL PLUGIN STYLES -->
 
         <?php if (ENV == "production") { ?>
-            <script>
-                // Google analytics
-            </script>			
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'<a href="https://www.googletagmanager.com/gtm.js?id=">https://www.googletagmanager.com/gtm.js?id=</a>'+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TM4ZP');</script>
+<!-- End Google Tag Manager -->	
 
         <?php } ?>  
     </head>
     <!-- END HEAD -->
 
     <body>
+	<?php if (ENV == "production") { ?>
+	<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'<a href="https://www.googletagmanager.com/gtm.js?id=">https://www.googletagmanager.com/gtm.js?id=</a>'+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TM4ZP');</script>
+<!-- End Google Tag Manager -->
+	<?php } ?>
 
         <div id="fb-root"></div>
         <script>(function (d, s, id) {
