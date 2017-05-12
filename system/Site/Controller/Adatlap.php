e91b71a82ca6a086954f16c4493634816569cafd
varnagy-zsolt <varnagyzsolt1975@gmail.com>
2017-05-12 13:17:43 +0200

    javitas - szoveg kodolas

--- system/Site/Controller/Adatlap.php
+++ system/Site/Controller/Adatlap.php
@@ -16,7 +16,7 @@ class Adatlap extends SiteController {
     
     /**
      * Ingatlan adatlap pdf generálása és küldése a böngészőnek
-     * 	
+     *  
      * @param $id integer   ingatlan id
      * @return void 
      */
@@ -154,17 +154,18 @@ class Adatlap extends SiteController {
         }
 
 
-        //		define('FPDF_FONTPATH','/home/www/font');
+        //      define('FPDF_FONTPATH','/home/www/font');
 
         //Instanciation of inherited class
         $pdf = new \FPDF();
   //      $pdf->AliasNbPages();
         $pdf->AddPage();
-        $pdf->AddFont('helvetica', '');
-        $pdf->AddFont('helveticab', '');
-        $pdf->SetFont('helveticab', '', 12);
-        
-         $pdf->Image('public/site_assets/images/logo.png', 10, 10, 25);
+        $pdf->AddFont('arial', '', 'arial.php');
+        $pdf->AddFont('arialb', '', 'arialb.php');
+        $pdf->SetFont('arialb', '', 12);
+
+
+        $pdf->Image('public/site_assets/images/logo.png', 10, 10, 25);
         $pdf->SetXY(50, 20);
         $pdf->SetDrawColor(200, 200, 200);
         
@@ -183,7 +184,7 @@ class Adatlap extends SiteController {
 
         //Set x and y position for the main text, reduce font size and write content
         $pdf->SetXY(10, 35);
-        $pdf->SetFont('helvetica', '', 10);
+        $pdf->SetFont('arial', '', 10);
 
 
 
@@ -191,22 +192,22 @@ class Adatlap extends SiteController {
         $pdf->Cell(0, 1, '', 0, 0, 'L', 1);
         $pdf->Ln(5);
 
-        $pdf->SetFont('helveticab', '', 11);
+        $pdf->SetFont('arialb', '', 11);
         $pdf->Cell(0, 5, $this->utf8_to_latin2_hun('Ref. szám: ' . $ingatlan['ref_num']), 0, 1, 'L', 0);
         $pdf->Cell(0, 5, $this->utf8_to_latin2_hun($elado) . ' | ' . $this->utf8_to_latin2_hun($ingatlan['kat_nev_' . LANG]) . ' | ' . $price, 0, 1, 'L', 0);
         $pdf->Ln(2);
-        $pdf->SetFont('helveticab', '', 13);
+        $pdf->SetFont('arialb', '', 13);
         $pdf->MultiCell(0, 8, $this->utf8_to_latin2_hun($ingatlan['ingatlan_nev_' . LANG]), 0, 'L', 0);
 
         $pdf->Ln(5);
-        $pdf->SetFont('helvetica', '', 9);
+        $pdf->SetFont('arial', '', 9);
         $pdf->MultiCell(90, 6, $this->utf8_to_latin2_hun($ingatlan['leiras_' . LANG]), 0, 'J', 0);
 
         $pdf->Ln(5);
 
-        $pdf->SetFont('helvetica', 'B', 9);
+        $pdf->SetFont('arial', 'B', 9);
         $pdf->Cell(0, 6, utf8_decode('Adatok:'), 0, 1, 'L', 0);
-        $pdf->SetFont('helvetica', '', 9);
+        $pdf->SetFont('arial', '', 9);
         $pdf->Cell(30, 5, utf8_decode('Elhelyezkedés:'), 0, 0, 'L', 0);
 
         if (isset($ingatlan['kerulet']) && ($ingatlan['utca_megjelenites'] == 1)) {
@@ -255,9 +256,9 @@ class Adatlap extends SiteController {
         $pdf->Ln(5);
 
 
-        $pdf->SetFont('helveticab', '', 9);
+        $pdf->SetFont('arialb', '', 9);
         $pdf->Cell(0, 5, $this->utf8_to_latin2_hun('Jellemzők:'), 0, 1, 'L', 0);
-        $pdf->SetFont('helvetica', '', 9);
+        $pdf->SetFont('arial', '', 9);
 
         /*         * ************ JELLEMZŐK ************** */
         $pdf->Cell(30, 5, $this->utf8_to_latin2_hun('Fűtés:'), 0, 0, 'L', 0);
@@ -285,13 +286,13 @@ class Adatlap extends SiteController {
         $pdf->Ln(5);
 
 
-        $pdf->SetFont('helveticab', '', 9);
+        $pdf->SetFont('arialb', '', 9);
 
 
 
 
         $pdf->MultiCell(0, 5, $this->utf8_to_latin2_hun('Extrák:'), 0, 'L', 0);
-        $pdf->SetFont('helvetica', '', 9);
+        $pdf->SetFont('arial', '', 9);
         $pdf->MultiCell(100, 5, $this->utf8_to_latin2_hun($extrak), 0, 'L', 0);
 
 
@@ -302,9 +303,9 @@ class Adatlap extends SiteController {
         $pdf->Ln(5);
 
 
-        $pdf->SetFont('helveticab', '', 9);
+        $pdf->SetFont('arialb', '', 9);
         $pdf->MultiCell(0, 5, $this->utf8_to_latin2_hun('További információért keresse ingatlan referensünket:'), 0, 'L', 0);
-        $pdf->SetFont('helvetica', '', 9);
+        $pdf->SetFont('arial', '', 9);
         $pdf->MultiCell(0, 5, $this->utf8_to_latin2_hun($agent['first_name']) . ' ' . $this->utf8_to_latin2_hun($agent['last_name']) . $this->utf8_to_latin2_hun(' | Tel: ') . $this->utf8_to_latin2_hun($agent['phone']), 0, 'L', 0);
         $pdf->MultiCell(0, 5, $this->utf8_to_latin2_hun('E-mail: ' . $this->utf8_to_latin2_hun($agent['email'])), 0, 'L', 0);
 
@@ -314,10 +315,19 @@ class Adatlap extends SiteController {
     }
 
     
-     public function utf8_to_latin2_hun($str) {
-        return str_replace(array("\xc3\xb6", "\xc3\xbc", "\xc3\xb3", "\xc5\x91", "\xc3\xba", "\xc3\xa9", "\xc3\xa1", "\xc5\xb1", "\xc3\xad", "\xc3\x96", "\xc3\x9c", "\xc3\x93", "\xc5\x90", "\xc3\x9a", "\xc3\x89", "\xc3\x81", "\xc5\xb0", "\xc3\x8d"), array("\xf6", "\xfc", "\xf3", "\xf5", "\xfa", "\xe9", "\xe1", "\xfb", "\xed", "\xd6", "\xdc", "\xd3", "\xd5", "\xda", "\xc9", "\xc1", "\xdb", "\xcd"), $str);
-    }   
+    public function utf8_to_latin2_hun($str)
+    {
+        return str_replace(
+            array( "\xc2\xa0", "\xc3\xb6", "\xc3\xbc", "\xc3\xb3", "\xc5\x91", "\xc3\xba", "\xc3\xa9", "\xc3\xa1", "\xc5\xb1", "\xc3\xad", "\xc3\x96", "\xc3\x9c", "\xc3\x93", "\xc5\x90", "\xc3\x9a", "\xc3\x89", "\xc3\x81", "\xc5\xb0", "\xc3\x8d"),
+            array( "\x20", "\xf6", "\xfc", "\xf3", "\xf5", "\xfa", "\xe9", "\xe1", "\xfb", "\xed", "\xd6", "\xdc", "\xd3", "\xd5", "\xda", "\xc9", "\xc1", "\xdb", "\xcd"),
+            $str
+            );
 
+        // U+00A0       \xc2\xa0    &#xA0;      NO-BREAK SPACE
+        // \x20 space karakter
+
+        // U+00C2   Â   \xc3\x82    &#xC2;  Â   LATIN CAPITAL LETTER A WITH CIRCUMFLEX        
+    }   
 
 }
 ?>
